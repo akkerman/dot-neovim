0a252d04fcf639f8549d90f4e97b634be2299565
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-11-24 14:06:28 +0100

    Add requires props to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,28 @@
-return { -- for syntax highlighting and more
-  "nvim-treesitter/nvim-treesitter",
-  run = ":TSUpdate",  -- Zorg ervoor dat de parser up-to-date is
+return {
+  -- for syntax highlighting and more
+  'nvim-treesitter/nvim-treesitter',
+  run = ':TSUpdate',  -- Zorg ervoor dat de parser up-to-date is
   config = function()
     require'nvim-treesitter.configs'.setup {
-      ensure_installed = { "javascript", "typescript", "json", "jsdoc", "python", "terraform", "yaml"},  -- Voeg talen toe die je nodig hebt
+      ensure_installed = {
+        'javascript',
+        'jsdoc',
+        'json',
+        'lua',
+        'python',
+        'query',
+        'terraform',
+        'typescript',
+        'vim',
+        'vimdoc',
+        'yaml',
+      },
+
+
+      auto_install = true,
+      sync_install = false,
+      ignore_install = {},
+      modules = {},
       highlight = {
         enable = true,  -- Zet syntax highlighting aan
         additional_vim_regex_highlighting = false,
