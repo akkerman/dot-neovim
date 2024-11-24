8157fce43008093b896e8cb8862bd9e6bb26f10b
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-11-24 16:00:33 +0100

    Add notification to custom mapping and lazy

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,14 +2,7 @@ local map = vim.keymap.set
 map('n', '<leader>n', ':set hlsearch!<CR>', { desc = '(dis)able search highlight' })
 map('n', '<leader>e', ":e <C-R>=expand('%:.:h') . '/'<CR>", { desc = 'edit other file in same dir as current' })
 map('n', '<leader>gf', ':e <cfile><cr>', { desc = 'edit file or create file under cursor' })
-map('n', '<leader>fc', ":let @+ = expand('%:p')<cr>", { desc = 'copy filepath to clipboard' })
-map('n', '<leader>=', 'mzgg=G`z', { desc = 'auto-indent entire file' })
-
--- Utility function for notifications
-local notify = function(msg, level)
-  level = level or vim.log.levels.INFO
-  vim.notify(msg, level, { title = "Action Completed" })
-end
+map('n', '<leader>F=', 'mzgg=G`z', { desc = 'auto-indent entire file' })
 
 -- Utility function for getting the selected range in visual mode
 local get_visual_selection = function()
@@ -25,29 +18,36 @@ local get_visual_selection = function()
   return start_line, start_col, end_line, end_col
 end
 
+map('n', '<leader>fc', function()
+  -- Copy file path to clipboard
+  vim.cmd('let @+ = expand("%:p")')
+  -- Show notification with the file name
+  vim.notify(vim.fn.expand('%:p'), vim.log.levels.INFO, { title = 'Copied file path to clipboard' })
+end, { desc = 'Copy file path to clipboard' })
+
 -- Format JSON
 map('n', '<leader>Fj', function()
   vim.cmd('silent! %!jq \'.\'')
   vim.bo.filetype = 'json'
-  notify('Formatted JSON (pretty print)')
+  vim.notify('Formatted JSON (pretty print)')
 end, { desc = 'Format JSON (pretty print)' })
 
 map('n', '<leader>FJ', function()
   vim.cmd('silent! %!jq \'.\' -c')
   vim.bo.filetype = 'json'
-  notify('Formatted JSON (compact)')
+  vim.notify('Formatted JSON (compact)')
 end, { desc = 'Format JSON (compact)' })
 
 -- Format XML
 map('n', '<leader>Fx', function()
   vim.cmd('silent! %!xmllint --format -')
-  notify('Formatted XML')
+  vim.notify('Formatted XML')
 end, { desc = 'Format XML' })
 
 -- Use silicon for generating images of code
 map('n', '<leader>ci', function()
   vim.cmd('silent! !silicon %')
-  notify('Generated code image (current file)')
+  vim.notify('Generated code image (current file)')
 end, { desc = 'Generate code image (current file)' })
 
 -- Generate a code image from a visual selection
@@ -55,13 +55,13 @@ vim.keymap.set('v', '<leader>ci', function()
   local start_line, _, end_line, _ = get_visual_selection()
   -- Run the silicon command for the selected range
   vim.cmd(string.format("silent! %d,%dwrite !silicon -l %s", start_line, end_line, vim.bo.filetype))
-  notify('Generated code image (selected)')
+  vim.notify('Generated code image (selected)')
 end, { desc = 'Generate code image (selected)' })
 
 -- Copy file content or selection to clipboard using xclip
 map('n', '<leader>cc', function()
   vim.cmd('silent! !xclip -selection clipboard %')
-  notify('Copied file content to clipboard')
+  vim.notify('Copied file content to clipboard')
 end, { desc = 'Copy file content to clipboard' })
 
 -- Copy selection to clipboard using xclip
@@ -69,11 +69,11 @@ vim.keymap.set('v', '<leader>cc', function()
   local start_line, _, end_line, _ = get_visual_selection()
   -- Run the xclip command for the selected range
   vim.cmd(string.format("silent! %d,%dw !xclip -selection clipboard", start_line, end_line))
-  notify('Copied selection to clipboard')
+  vim.notify('Copied selection to clipboard')
 end, { desc = 'Copy selection to clipboard' })
 
 -- Copy file to ~/Downloads/code
 map('n', '<leader>cp', function()
   vim.cmd('silent! !cp % ~/Downloads/code')
-  notify('Copied file to ~/Downloads/code')
+  vim.notify('Copied file to ~/Downloads/code')
 end, { desc = 'Copy file to ~/Downloads/code' })
