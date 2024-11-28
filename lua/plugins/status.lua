9dcffca5ff71817d7ef32afb0c92334675fb4c0c
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-11-28 07:08:00 +0100

    Add status line

--- lua/plugins/status.lua
+++ lua/plugins/status.lua
@@ -1,4 +1,4 @@
-return { 
+return {
   {
     'gcmt/taboo.vim',
     config = function()
@@ -6,6 +6,21 @@ return {
       vim.g.taboo_tab_format = ' %m%U%F'
       vim.g.taboo_renamed_tab_format = ' %m%U%l'
       vim.g.taboo_modified_tab_flag = '●'
+    end
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup {
+        options = {
+          -- theme = 'gruvbox',
+          globalstatus = true,
+        },
+        sections = { lualine_a = {
+          { 'mode', fmt = function(str) return str:sub(1,1) end } },
+        },
+      }
     end
   }
 }
