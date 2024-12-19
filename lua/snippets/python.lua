da7829f6655a4797a2f6aa35703171df6fed4526
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-12-19 08:18:24 +0100

    Update python aoc snippet

--- lua/snippets/python.lua
+++ lua/snippets/python.lua
@@ -1,14 +1,20 @@
 local ls = require("luasnip")
 local s = ls.snippet
-local t = ls.text_node
 local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
 
 ls.add_snippets("python", {
-  s({ trig="aoc", dscr="Create advent of code starting point"}, 
+  s({ trig="aoc", dscr="Create advent of code starting point"},
   fmt(
   [[
-import sys
-import string
+import fileinput
+import heapq
+import re
+from collections import deque, defaultdict, Counter
+from functools import lru_cache
+
+
+from utils import perf_timer
 
 def part_one(lines):
     """Solution to part one."""
@@ -23,7 +29,7 @@ def part_two(lines):
 def main():
     """Parse input file, pass to puzzle solvers."""
     lines = []
-    for line in sys.stdin:
+    for line in fileinput.input():
         line = line.strip()
         line = list(map(int, line.split(',')))
        	<>
