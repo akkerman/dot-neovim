ecde1f90cb55f2cfdb36f4cf71e3ba0cde3225f1
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2024-12-30 09:21:26 +0100

    Disable tmux-runner
    Kan wellicht weg.
    Heeft meer mogelijkheden dan vim-slime maar is ook ingewikkelder

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    "christoomey/vim-tmux-runner",
-    lazy = false,
-    config = function ()
-      vim.api.nvim_set_keymap('v', '<leader>rr', ':VtrSendLinesToRunner<CR>', { noremap = true, silent = true, desc = "Send selected lines to tmux runner" })
-      vim.api.nvim_set_keymap('n', '<leader>rr', ':VtrSendLinesToRunner<CR>', { noremap = true, silent = true, desc = "Send selected lines to tmux runner" })
-      vim.api.nvim_set_keymap('n', '<leader>rs', ':VtrSendCommandToRunner<CR>', { noremap = true, silent = true, desc = "Send a command to tmux runner" })
-      vim.api.nvim_set_keymap('n', '<leader>rl', ':VtrOpenRunner<CR>', { noremap = true, silent = true, desc = "Open a tmux runner pane" })
-    end
-  },
+  -- {
+  --   "christoomey/vim-tmux-runner",
+  --   lazy = false,
+  --   config = function ()
+  --     vim.api.nvim_set_keymap('v', '<leader>rr', ':VtrSendLinesToRunner<CR>', { noremap = true, silent = true, desc = "Send selected lines to tmux runner" })
+  --     vim.api.nvim_set_keymap('n', '<leader>rr', ':VtrSendLinesToRunner<CR>', { noremap = true, silent = true, desc = "Send selected lines to tmux runner" })
+  --     vim.api.nvim_set_keymap('n', '<leader>rs', ':VtrSendCommandToRunner<CR>', { noremap = true, silent = true, desc = "Send a command to tmux runner" })
+  --     vim.api.nvim_set_keymap('n', '<leader>rl', ':VtrOpenRunner<CR>', { noremap = true, silent = true, desc = "Open a tmux runner pane" })
+  --   end
+  -- },
   {
     'jpalardy/vim-slime',
     lazy = false,
