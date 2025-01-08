3706c77b254abc302a21148b92f37a45f20bed1a
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2025-01-08 13:02:35 +0100

    Install tabular

--- lua/plugins/notities.lua
+++ lua/plugins/notities.lua
@@ -4,5 +4,8 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
+  },
+  {
+    "godlygeek/tabular",
   }
 }
