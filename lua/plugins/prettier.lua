0de8f58c635e2f63695817818933f9b264ffd518
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2025-02-18 10:07:20 +0100

    Add prettier

---
+++ lua/plugins/prettier.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescript = { "prettierd" },
+        typescriptreact = { "prettierd" },
+        json = { "prettierd" },
+        yaml = { "prettierd" },
+        markdown = { "prettierd" },
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    },
+  }
+}
