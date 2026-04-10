76a83c06427184043abf13cc755a0f593a3866fa
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2026-04-10 15:59:14 +0200

    Add some colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -33,4 +33,25 @@ return {
       end
     end,
   },
+  { "ThorstenRhau/token" },
+  {
+    "kungfusheep/mfd.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      if colors == "mfd-stealth" then
+        vim.cmd('colorscheme mfd-stealth')
+
+        vim.opt.guicursor = {
+          "n:block-CursorNormal",
+          "v:block-CursorVisual",
+          "i:block-CursorInsert",
+          "r-cr:block-CursorReplace",
+          "c:block-CursorCommand",
+        }
+
+        require('mfd').enable_cursor_sync()
+      end
+    end,
+  }
 }
